0511acf94aced393fba49ba9b6e5999140d8dd34
Alon Luboshitz <117107259+AlonLuboshitz@users.noreply.github.com>
2024-06-06 16:09:49 +0300

    Add files via upload

---
+++ Exercise 1/pgn_split.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <source_pgn_file> <destination_directory>"
+    exit 1
+fi
+pgn_path="$1"
+dest_dir="$2"
+if [ ! -e "$pgn_path" ]; then
+    echo "Error: File '$input_file' does not exist."
+    exit 1
+fi
+if [ ! -d "$dest_dir" ]; then
+    echo "Created directory '$dest_dir'."
+    mkdir -p "$dest_dir"
+fi
+# Extracting the prefix until ".pgn"
+prefix=$(basename "$pgn_path" .pgn)
+game_num=1
+suffix=".pgn"
+output="${prefix}_${game_num}${suffix}"
+line_count=0
+while read -r line; do
+    if [[ "$line" =~ ^$ ]] || [[ "$line" =~ ^[[:space:]]+$ ]]; then
+        ((line_count++))
+        
+
+    elif [ $line_count -eq 2 ]; then
+        ((game_num++))
+        echo "Saved game to '$dest_dir/$output'."
+        output="${prefix}_${game_num}${suffix}"
+
+        line_count=0
+    elif [[ "$line" =~ ^$ ]] || [[ "$line" =~ ^[[:space:]]+$ ]] ; then
+        
+    fi
+    else
+        echo "$line" >> "$dest_dir/$output"
+    fi
+    # if [ -z "$line" ]; then 
+    #     ((line_count++))
+    # elif [ $line_count -eq 2 ]; then
+            
+    #     fi
+    # fi
+done < "$pgn_path"
+#"/home/alon/Op_sys/Operation-Systems-Exercises/Exercise 1/pgns/capmemel24.pgn" "allala"
